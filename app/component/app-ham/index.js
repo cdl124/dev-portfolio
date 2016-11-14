17347850d6e94787b3ef20b06f9e50b21c834c2d
Cortney Lane <cdlane124@gmail.com>
2016-11-13 21:29:04 -0800

    app-nav is functional, but still need styling on pop-out menu

--- app/component/app-ham/index.js
+++ app/component/app-ham/index.js
@@ -7,4 +7,10 @@ angular.module('cortFolio')
 
 .component('appHam', {
   template: require('./app-ham.html'),
-});
+})
+.controller('HamController', '$log', HamController);
+
+function HamController($log) {
+  $log.debug('HamController invoked');
+  this.showNav = false;
+}
