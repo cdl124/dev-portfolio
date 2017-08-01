022aad2c36f750d90b22f2702448f669e7fe0aca
Cortney Lane <cdlane124@gmail.com>
2017-07-31 21:07:33 -0700

    app now runs on localhost:8080 and 3000

--- server.js
+++ server.js
@@ -2,13 +2,14 @@
 
 const port = process.env.PORT || 3000;
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static(`${__dirname}/app`));
+app.use(express.static(`${__dirname}/build`));
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(port, (err) => {
